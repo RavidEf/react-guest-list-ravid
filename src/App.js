49a8f194bb4d864b99d3f75b635014cd4ed849f0
Ravid Efroni <ravidefroni@gmail.com>
2024-09-26 18:48:33 +0200

    Add useEffect functions

--- src/App.js
+++ src/App.js
@@ -7,6 +7,46 @@ export default function App() {
   const [guestLName, setGuestLName] = useState([]);
   const [attending, setAttending] = useState(false);
 
+  const baseUrl = 'http://localhost:3002';
+
+  useEffect(() => {
+    async function postGuests() {
+      const response = await fetch(`${baseUrl}/guests`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: function createNewGuests() {
+          setGuests([
+            ...guests,
+            {
+              firstName: guestFName,
+              lastName: guestLName,
+              id: guests.length > 0 ? guests[guests.length - 1].id + 1 : 1,
+              attending: attending,
+            },
+          ]);
+        },
+      });
+      const createdGuest = await response.json();
+
+      postGuests().catch((error) => {
+        console.log(error);
+      });
+    }
+  }, [guests]);
+
+  useEffect(() => {
+    async function fetchGuests() {
+      const response = await fetch(`${baseUrl}/guests`);
+      const allGuests = await response.json();
+      console.log(allGuests);
+    }
+    fetchGuests().catch((error) => {
+      console.log(error);
+    });
+  }, []);
+
   function createNewGuests() {
     setGuests([
       ...guests,
@@ -24,7 +64,6 @@ export default function App() {
     newGuests.pop();
     setGuests(newGuests);
   }
-  //console.log(guests);
 
   return (
     <main className="App">
@@ -63,7 +102,7 @@ export default function App() {
 
           <label> Attending?</label>
           <input
-            aria-label="attending"
+            aria-label="attending "
             type="checkbox"
             checked={attending}
             onChange={(event) => {
