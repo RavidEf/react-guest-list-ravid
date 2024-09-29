511725b995aaa1c06a7c468da75bb22a3c30bb37
Ravid Efroni <ravidefroni@gmail.com>
2024-09-29 22:02:53 +0200

    Add loading state and with conditional rendering

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ export default function App() {
   const [guestFName, setGuestFName] = useState([]);
   const [guestLName, setGuestLName] = useState([]);
   const [isAttending, setIsAttending] = useState(false);
+  const [loading, setLoading] = useState(true);
 
   const baseUrl = 'http://localhost:4000';
 
@@ -35,11 +36,12 @@ export default function App() {
       const response = await fetch(`${baseUrl}/guests/`);
       const allGuests = await response.json();
       setGuests(allGuests);
+      setLoading(false);
     }
     fetchGuests().catch((error) => {
       console.log(error);
     });
-  }, [guests, guestFName, guestLName, isAttending]);
+  }, []);
 
   // DELETE !!
   async function deleteGuestim(id) {
@@ -50,6 +52,20 @@ export default function App() {
     setGuests(guests.filter((guest) => guest.id !== id));
   }
 
+  // Update API function
+  async function updateGuests({ id }) {
+    const response = await fetch(`${baseUrl}/guests/${id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ attending: isAttending }),
+    });
+    const updatedGuest = await response.json();
+    setGuests([...guests, updatedGuest]);
+  }
+
+  // Create new guests in the UI
   function createNewGuests() {
     setGuests([
       ...guests,
@@ -57,7 +73,7 @@ export default function App() {
         firstName: guestFName,
         lastName: guestLName,
         id: guests.length > 0 ? guests[guests.length - 1].id + 1 : 1,
-        isAttending: isAttending,
+        isAttending: false,
       },
     ]);
   }
@@ -72,7 +88,7 @@ export default function App() {
   return (
     <main className="App">
       <div>
-        <h1>Welcmoe to the exclusive Guest List</h1>
+        <h1>Welcmoe to the fun Guest List</h1>
         <h4>Enter a guest name and press Enter on the last input field</h4>
         <form onSubmit={handelSubmit}>
           <label htmlFor="first_name"> First name</label>
@@ -100,35 +116,50 @@ export default function App() {
           />
           <br />
 
-          <button>Create User</button>
+          <button id="create">Create User</button>
         </form>
       </div>
-      <div className="guestContainer" data-test-id="guest">
-        {guests.map((guest) => {
-          return (
-            <div key={`guest-${guest.id}`} className="guestBlock">
-              <h2>
-                {guest.firstName} {guest.lastName}
-              </h2>
 
-              <p>guest number: {guest.id}</p>
-              <label htmlFor={`is_attending_${guest.id}`}> Attending?</label>
-              <input
-                id={`is_attending_${guest.id}`}
-                aria-label="attending"
-                type="checkbox"
-                checked={guest.isAttending}
-                onChange={(event) => {
-                  setIsAttending(event.currentTarget.checked);
-                }}
-              />
-              <button onClick={() => deleteGuestim(guest.id)}>
-                Delete guest
-              </button>
-            </div>
-          );
-        })}
-      </div>
+      <section>
+        {loading ? (
+          <div>
+            <br />
+            <br />
+            <h1>Loading the Guest List...</h1>
+          </div>
+        ) : (
+          <div className="guestContainer" data-test-id="guest">
+            {guests.map((guest) => {
+              return (
+                <div key={`guest-${guest.id}`} className="guestBlock">
+                  <h2>
+                    {guest.firstName} {guest.lastName}
+                  </h2>
+
+                  <p>guest number: {guest.id}</p>
+                  <label htmlFor="is_attending"> Attending?</label>
+                  <input
+                    id="is_attending"
+                    aria-label="attending"
+                    type="checkbox"
+                    checked={guest.isAttending}
+                    onChange={async (event) => {
+                      setIsAttending(event.currentTarget.checked);
+                      await updateGuests(event.currentTarget.checked);
+                    }}
+                  />
+                  <button
+                    className="delete"
+                    onClick={() => deleteGuestim(guest.id)}
+                  >
+                    Delete guest
+                  </button>
+                </div>
+              );
+            })}
+          </div>
+        )}
+      </section>
     </main>
   );
 }
